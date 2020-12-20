31fe96dc5d1b7bdde7726563baaa54f152c72c2b
pospelovav <shnurokvtaige@yandex.ru>
2020-12-20 22:49:31 +0300

    настроена дата, дневная-ночная темы, ориентация экрана

--- app/src/main/java/com/geekbrains/androidbasiclevel/pospelov/MainActivity.java
+++ app/src/main/java/com/geekbrains/androidbasiclevel/pospelov/MainActivity.java
@@ -1,14 +1,39 @@
 package com.geekbrains.androidbasiclevel.pospelov;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("WrongConstant")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView city = findViewById(R.id.city);
+        city.setText("Moscow");
+        TextView textTempToday = findViewById(R.id.textTempToday);
+        textTempToday.setText("-4 °C");
+        TextView textTempTomorrow = findViewById(R.id.textTempTomorrow);
+        textTempTomorrow.setText("0 °C");
+        TextView textTodayDate = findViewById(R.id.textTodayDate);
+        Calendar today = new GregorianCalendar();
+        DateFormat df = new SimpleDateFormat("E, dd MMM yyy");
+        textTodayDate.setText(df.format(today.getTime()));
+        TextView textTomorrowDate = findViewById(R.id.textTomorrowDate);
+        today.roll(Calendar.DAY_OF_MONTH, +1);
+        textTomorrowDate.setText(df.format(today.getTime()));
+
     }
 }

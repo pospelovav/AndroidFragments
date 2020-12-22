6d069faca4b6301034271342dd7c431b38cef62e
pospelovav <shnurokvtaige@yandex.ru>
2020-12-22 23:15:42 +0300

    AutoCompleteTextView + ButtonSelect

--- app/src/main/java/com/geekbrains/androidbasiclevel/pospelov/SettingsActivity.java
+++ app/src/main/java/com/geekbrains/androidbasiclevel/pospelov/SettingsActivity.java
@@ -2,13 +2,39 @@ package com.geekbrains.androidbasiclevel.pospelov;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
 
-public class SettingsActivity extends AppCompatActivity {
+public class SettingsActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        AutoCompleteTextView textView = (AutoCompleteTextView) findViewById(R.id.textChangeCityView);
+        String[] countries = getResources().getStringArray(R.array.city_array);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, countries);
+        textView.setAdapter(adapter);
+
+        Button btnSelect;
+        btnSelect = (Button) findViewById(R.id.buttonSelect);
+        btnSelect.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.buttonSelect:
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
     }
 }

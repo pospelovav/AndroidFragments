a56d4501655894d630f9c6bb7943f01bf945232a
pospelovav <shnurokvtaige@yandex.ru>
2021-01-14 21:46:59 +0300

    Убраны toast и log Исправлено замечание: после выбора дополнительных параметров, на экране ничего не изменяется, между активностями передается только название города

--- app/src/main/java/com/geekbrains/androidbasiclevel/pospelov/MainActivity.java
+++ app/src/main/java/com/geekbrains/androidbasiclevel/pospelov/MainActivity.java
@@ -8,12 +8,10 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.view.View.OnClickListener;
-import android.widget.Toast;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -21,8 +19,19 @@ import java.util.GregorianCalendar;
 
 public class MainActivity extends AppCompatActivity implements OnClickListener {
     public static String theme = "LIGHT";
-    private static final String TAG = "MAIN_ACTIVITY";
     TextView textViewCity;
+    TextView textViewWindSpeedToday;
+    TextView textViewAirPressureToday;
+    TextView airPressureTodayView;
+    TextView windSpeedTodayView;
+    TextView textViewWindSpeedTomorrow;
+    TextView windSpeedTomorrowView;
+    TextView textViewAirPressureTomorrow;
+    TextView airPressureTomorrowView;
+    TextView textTempToday;
+    TextView textTempTomorrow;
+    MainPresenter presenter = MainPresenter.getInstance();
+
     private final int settingsActivityRequestCode = 1234;
 
     @SuppressLint("SetTextI18n")
@@ -49,67 +58,33 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
         textTomorrowDate.setText(df.format(today.getTime()));
 
         ImageButton btnSettings;
-        btnSettings = (ImageButton) findViewById(R.id.buttonSettings);
+        btnSettings = findViewById(R.id.buttonSettings);
         btnSettings.setOnClickListener(this);
         ImageButton btnInfo;
-        btnInfo = (ImageButton) findViewById(R.id.imageButtonInfo);
+        btnInfo = findViewById(R.id.imageButtonInfo);
         btnInfo.setOnClickListener(this);
 
-        textViewCity = (TextView) findViewById(R.id.cityView);
-        final TextView textViewWindSpeedToday = findViewById(R.id.textViewWindSpeedToday);
-        final TextView windSpeedTodayView = findViewById(R.id.windSpeedTodayView);
-        final TextView textViewAirPressureToday = findViewById(R.id.textViewAirPressureToday);
-        final TextView airPressureTodayView = findViewById(R.id.airPressureTodayView);
-        final TextView textViewWindSpeedTomorrow = findViewById(R.id.textViewWindSpeedTomorrow);
-        final TextView windSpeedTomorrowView = findViewById(R.id.windSpeedTomorrowView);
-        final TextView textViewAirPressureTomorrow = findViewById(R.id.textViewAirPressureTomorrow);
-        final TextView airPressureTomorrowView = findViewById(R.id.airPressureTomorrowView);
-        final TextView textTempToday = findViewById(R.id.textTempTodayView);
-        final TextView textTempTomorrow = findViewById(R.id.textTempTomorrowView);
-        final MainPresenter presenter = MainPresenter.getInstance();
+        textViewCity = findViewById(R.id.cityView);
+        textViewWindSpeedToday =  findViewById(R.id.textViewWindSpeedToday);
+        windSpeedTodayView =  findViewById(R.id.windSpeedTodayView);
+        textViewAirPressureToday = findViewById(R.id.textViewAirPressureToday);
+        airPressureTodayView = findViewById(R.id.airPressureTodayView);
+        textViewWindSpeedTomorrow = findViewById(R.id.textViewWindSpeedTomorrow);
+        windSpeedTomorrowView = findViewById(R.id.windSpeedTomorrowView);
+        textViewAirPressureTomorrow = findViewById(R.id.textViewAirPressureTomorrow);
+        airPressureTomorrowView = findViewById(R.id.airPressureTomorrowView);
+        textTempToday = findViewById(R.id.textTempTodayView);
+        textTempTomorrow = findViewById(R.id.textTempTomorrowView);
 
         textTempToday.setText(presenter.getDegreesToday() + " °C");
         textTempTomorrow.setText(presenter.getDegreesTomorrow() + " °C");
 
-        if (presenter.isCheckWindSpeed()){
-            textViewWindSpeedToday.setVisibility(View.VISIBLE);
-            windSpeedTodayView.setVisibility(View.VISIBLE);
-            textViewWindSpeedTomorrow.setVisibility(View.VISIBLE);
-            windSpeedTomorrowView.setVisibility(View.VISIBLE);
-        } else {
-            textViewWindSpeedToday.setVisibility(View.GONE);
-            windSpeedTodayView.setVisibility(View.GONE);
-            textViewWindSpeedTomorrow.setVisibility(View.GONE);
-            windSpeedTomorrowView.setVisibility(View.GONE);
-        }
-        if (presenter.isCheckAtmosphericPressure()){
-            textViewAirPressureToday.setVisibility(View.VISIBLE);
-            airPressureTodayView.setVisibility(View.VISIBLE);
-            textViewAirPressureTomorrow.setVisibility(View.VISIBLE);
-            airPressureTomorrowView.setVisibility(View.VISIBLE);
-        } else {
-            textViewAirPressureToday.setVisibility(View.GONE);
-            airPressureTodayView.setVisibility(View.GONE);
-            textViewAirPressureTomorrow.setVisibility(View.GONE);
-            airPressureTomorrowView.setVisibility(View.GONE);
-        }
         if (presenter.getCityName() != null){
             textViewCity.setText(presenter.getCityName());
         } else {
             String[] citys = getResources().getStringArray(R.array.city_array);
             textViewCity.setText(citys[0]);
         }
-
-        String instanceState;
-        if (savedInstanceState == null){
-            instanceState = "Fist start " + TAG;
-        }
-        else{
-            instanceState = "Restart " + TAG;
-        }
-        Toast.makeText(getApplicationContext(), instanceState + " - onCreate()", Toast.LENGTH_LONG).show();
-        Log.d(TAG, instanceState + " - onCreate()");
-
     }
 
     @SuppressLint("NonConstantResourceId")
@@ -140,59 +115,35 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
                 cityNameFromSettings = dataFromSettingsActivity.getStringExtra("cityName");
                 if (!cityNameFromSettings.equals("")) {  //если пользователь не ввел город
                     textViewCity.setText(cityNameFromSettings);
+                    presenter.setCityName(cityNameFromSettings);   //для сохранения города при переключении на дневную/ночную тему
+                } else if (presenter.getCityName() != null){
+                    textViewCity.setText(presenter.getCityName());
                 }
             }
         }
+        if (presenter.isCheckWindSpeed()){
+            textViewWindSpeedToday.setVisibility(View.VISIBLE);
+            windSpeedTodayView.setVisibility(View.VISIBLE);
+            textViewWindSpeedTomorrow.setVisibility(View.VISIBLE);
+            windSpeedTomorrowView.setVisibility(View.VISIBLE);
+        } else {
+            textViewWindSpeedToday.setVisibility(View.GONE);
+            windSpeedTodayView.setVisibility(View.GONE);
+            textViewWindSpeedTomorrow.setVisibility(View.GONE);
+            windSpeedTomorrowView.setVisibility(View.GONE);
+        }
+        if (presenter.isCheckAtmosphericPressure()){
+            textViewAirPressureToday.setVisibility(View.VISIBLE);
+            airPressureTodayView.setVisibility(View.VISIBLE);
+            textViewAirPressureTomorrow.setVisibility(View.VISIBLE);
+            airPressureTomorrowView.setVisibility(View.VISIBLE);
+        } else {
+            textViewAirPressureToday.setVisibility(View.GONE);
+            airPressureTodayView.setVisibility(View.GONE);
+            textViewAirPressureTomorrow.setVisibility(View.GONE);
+            airPressureTomorrowView.setVisibility(View.GONE);
+        }
         super.onActivityResult(requestCode, resultCode, dataFromSettingsActivity);
-    }
-
-    @Override
-    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
-        super.onRestoreInstanceState(savedInstanceState);
-        Toast.makeText(getApplicationContext(), TAG + " onRestoreInstanceState()", Toast.LENGTH_LONG).show();
-        Log.d(TAG, "onRestoreInstanceState()");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Toast.makeText(getApplicationContext(), TAG + " onStop()", Toast.LENGTH_LONG).show();
-        Log.d(TAG, "onStop()");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Toast.makeText(getApplicationContext(), TAG + " onResume()", Toast.LENGTH_LONG).show();
-        Log.d(TAG, "onResume()");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Toast.makeText(getApplicationContext(), TAG + " onPause()", Toast.LENGTH_LONG).show();
-        Log.d(TAG, "onPause()");
-    }
-
-    @Override
-    protected void onSaveInstanceState(@NonNull Bundle outState) {
-        super.onSaveInstanceState(outState);
-        Toast.makeText(getApplicationContext(), TAG + " onSaveInstanceState()", Toast.LENGTH_LONG).show();
-        Log.d(TAG, "onSaveInstanceState()");
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Toast.makeText(getApplicationContext(), TAG + " onRestart()", Toast.LENGTH_LONG).show();
-        Log.d(TAG, "onRestart()");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Toast.makeText(getApplicationContext(), TAG + " onDestroy()", Toast.LENGTH_LONG).show();
-        Log.d(TAG, "onDestroy()");
     }
 
 }
--- app/src/main/java/com/geekbrains/androidbasiclevel/pospelov/SettingsActivity.java
+++ app/src/main/java/com/geekbrains/androidbasiclevel/pospelov/SettingsActivity.java
@@ -6,19 +6,17 @@ import androidx.appcompat.app.AppCompatDelegate;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.Toast;
+
 
 public class SettingsActivity extends AppCompatActivity implements View.OnClickListener {
     private AutoCompleteTextView textView;
     private CheckBox checkWindSpeed;
     private CheckBox checkAtmosphericPressure;
-    private static final String TAG = "SETTINGS_ACTIVITY";
     final MainPresenter presenter = MainPresenter.getInstance();
 
     @Override
@@ -49,16 +47,6 @@ public class SettingsActivity extends AppCompatActivity implements View.OnClickL
             btnTheme.setText(getString(R.string.buttonLightText));
         }
         btnTheme.setOnClickListener(this);
-
-        String instanceState;
-        if (savedInstanceState == null){
-            instanceState = "Fist start " + TAG;
-        }
-        else{
-            instanceState = "Restart " + TAG;
-        }
-        Toast.makeText(getApplicationContext(), instanceState + " - onCreate()", Toast.LENGTH_LONG).show();
-        Log.d(TAG, instanceState + " - onCreate()");
     }
 
     @SuppressLint("NonConstantResourceId")
@@ -80,7 +68,6 @@ public class SettingsActivity extends AppCompatActivity implements View.OnClickL
                 String cityName = this.textView.getText().toString();       //передача введенного пользователем города в MainActivity
                 intentMainActivity.putExtra("cityName", cityName);
                 setResult(RESULT_OK, intentMainActivity);
-                presenter.setCityName(cityName);   //для сохранения города при переключении на дневную/ночную тему
                 finish();
                 break;
             case R.id.buttonDarkOrLight:                    //смена темы приложения
@@ -97,55 +84,6 @@ public class SettingsActivity extends AppCompatActivity implements View.OnClickL
             default:
                 break;
         }
-    }
-
-    @Override
-    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
-        super.onRestoreInstanceState(savedInstanceState);
-        Toast.makeText(getApplicationContext(), TAG + " onRestoreInstanceState()", Toast.LENGTH_LONG).show();
-        Log.d(TAG, "onRestoreInstanceState()");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Toast.makeText(getApplicationContext(), TAG + " onStop()", Toast.LENGTH_LONG).show();
-        Log.d(TAG, "onStop()");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Toast.makeText(getApplicationContext(), TAG + " onResume()", Toast.LENGTH_LONG).show();
-        Log.d(TAG, "onResume()");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Toast.makeText(getApplicationContext(), TAG + " onPause()", Toast.LENGTH_LONG).show();
-        Log.d(TAG, "onPause()");
-    }
-
-    @Override
-    protected void onSaveInstanceState(@NonNull Bundle outState) {
-        super.onSaveInstanceState(outState);
-        Toast.makeText(getApplicationContext(), TAG + " onSaveInstanceState()", Toast.LENGTH_LONG).show();
-        Log.d(TAG, "onSaveInstanceState()");
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Toast.makeText(getApplicationContext(), TAG + " onRestart()", Toast.LENGTH_LONG).show();
-        Log.d(TAG, "onRestart()");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Toast.makeText(getApplicationContext(), TAG + " onDestroy()", Toast.LENGTH_LONG).show();
-        Log.d(TAG, "onDestroy()");
     }
 
 }
